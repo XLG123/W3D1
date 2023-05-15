ef0c67f890cdd15d457f867bde48b4d7aa03ce7f
XLG123 <xiaolin.guan123@gmail.com>
2023-05-15 17:45:15 -0400

    Array partially completed

--- Enumerables/enumerables_array.rb
+++ Enumerables/enumerables_array.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class Array
   def my_each(&prc)
     i = 0
@@ -62,16 +64,28 @@ class Array
   end
 
   def my_zip(*arrays)
-    new_array = Array.new(self.length)
-    arrays.each_with_index do |array,index|
-      new_array[index] << array[]
-
+    result = []
+    self.each_with_index do |ele, idx|
+      result << [ele]
+      arrays.each do |sub_array|
+        result[idx] << sub_array[idx]
+      end
+    end
+    result
   end
 end
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten
 
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
 
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
 
+# p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten
 
 # return_value = [1, 2, 3].my_each do |num|
 #  puts num
